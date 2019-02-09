0440c158c8e281dd55d9961e1dd868bdc6be8647
Administrator <Administrator@USER-PC>
2019-02-09 22:49:49 +0530

    1st example ready to push

---
+++ todaydemo/src/page/gitexample1.java
@@ -0,0 +1,11 @@
+package page;
+
+public class gitexample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("gitexample1 display");
+
+	}
+
+}
